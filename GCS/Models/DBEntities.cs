4b7a0bd9f6467ca8c36f364c8bb0564e811b20e6
w3s <info@w3s.nl>
2018-05-07 11:15:44 +0200

    Use EF (as included in core umbraco) for the repositories

--- GCS/Models/DBEntities.cs
+++ GCS/Models/DBEntities.cs
@@ -1,13 +1,13 @@
-﻿//using System.Data.Entity;
-//using W3S_GCS.Models.Dtos;
+﻿using System.Data.Entity;
+using W3S_GCS.Models.Dtos;
 
-//namespace W3S_GCS.Models {
-//    public class DBEntities : DbContext {
-//        public DBEntities() : base("name=umbracoDbDSN") {
+namespace W3S_GCS.Models {
+    public class DBEntities : DbContext {
+        public DBEntities() : base("name=umbracoDbDSN") {
 
-//        }
-//        public virtual DbSet<SearchInstance> SearchInstance { get; set; }
-//        public virtual DbSet<SearchSettings> SearchSettings { get; set; }
-//        public virtual DbSet<SearchEntry> SearchEntries { get; set; }
-//    }
-//}
+        }
+        public virtual DbSet<SearchInstance> SearchInstance { get; set; }
+        public virtual DbSet<SearchSettings> SearchSettings { get; set; }
+        public virtual DbSet<SearchEntry> SearchEntries { get; set; }
+    }
+}
